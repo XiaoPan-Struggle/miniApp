bb9193d4d8c33c7b140647716bcfb5385c2dd324
XiaoPan <2489964425@qq.com>
2020-07-10 22:30:18 +0800

    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,65 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+import { request } from '../../request/index';
+//Page Object
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    // 轮播图数据
+    swiperList: [],
+    //导航列表数据
+    catesList:[],
+    //获取楼层数据
+    floorList:[]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  //options(Object)
+  // 页面开始加载就会触发
+  onLoad: function (options) {
+    // 发送异步请求获取数据
+    // 轮播图
+    this.getSwiperList()
+    // 导航列表
+    this.getCatesList()
+    // 楼层
+    this.getFloorList()
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
+  // 获取轮播图数据据
+  getSwiperList () {
+    request({
+      url:"https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"
+    }).then(result => {
       this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        swiperList: result.data.message
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
-  }
-})
+  },
+  // 获取导航列表数据
+  getCatesList () {
+    request({
+      url:"https://api-hmugo-web.itheima.net/api/public/v1/home/catitems"
+    }).then(result => {
+      this.setData({
+        catesList: result.data.message
+      })
+    })
+  },
+  // 获取导航列表数据
+  getFloorList () {
+    request({
+      url:"https://api-hmugo-web.itheima.net/api/public/v1/home/floordata"
+    }).then(result => {
+      this.setData({
+        floorList: result.data.message
+      })
+    })
+  },
+
+  
+  onReady: function () {},
+  onShow: function () {},
+  onHide: function () {},
+  onUnload: function () {},
+  onPullDownRefresh: function () {},
+  onReachBottom: function () {},
+  onShareAppMessage: function () {},
+  onPageScroll: function () {},
+  //item(index,pagePath,text)
+  onTabItemTap: function (item) {},
+});
